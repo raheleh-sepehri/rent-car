79f35eb1a8979ba542be6449e44241f6215ecfce
Raheleh sepehri <raheleh1392@gmail.com>
2023-08-09 20:20:02 +0330

    develope left side of Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,24 @@
+'use client'
+
+import { CustomButton } from ".";
 
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
 
-export default Hero
+    const handleScroll=()=>{}
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book and rent a car --quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+            Streamline your car rental experience with our effortless booking process.
+        </p>
+        <CustomButton handleClick={handleScroll} title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10"/>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
