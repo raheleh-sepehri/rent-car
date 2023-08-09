ca9414067389dbdd0eb70bedc88f676cd42d0d57
Raheleh sepehri <raheleh1392@gmail.com>
2023-08-09 21:13:29 +0330

    develope Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import { CustomButton } from ".";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="w-full absolute z-10 ">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center padding-x py-4">
+        <Link href="/" className="flex items-center justify-center">
+          <Image
+            src="./logo.svg"
+            alt="CarHub logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
+        <CustomButton
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+          title="Sign In"
+          btnType="button"
+        />
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
