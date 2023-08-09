541e5ee7f800c9b5a9168ccce024e5fa36af6031
Raheleh sepehri <raheleh1392@gmail.com>
2023-08-09 19:22:43 +0330

    import Hero in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import {Hero} from "@/components";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="overflow-hidden w-full h-screen">hii</main>;
+  return <main className="overflow-hidden w-full h-screen">
+    <Hero/>
+  </main>;
 }
