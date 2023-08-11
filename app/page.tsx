052d76d66e684f5d38c256a1f681bbba05b2e851
Raheleh sepehri <raheleh1392@gmail.com>
2023-08-11 19:16:05 +0330

    create  SearchBar and CustomFilter and add to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-import { Hero } from "@/components";
+import { CustomFilter, Hero, SearchBar } from "@/components";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden w-full">
       <Hero />
+      <div className="padding-x padding-y mt-12 max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like.</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar/>
+          <div className="home__filter-container">
+            <CustomFilter title="fuel"/>
+            <CustomFilter title="year"/>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
