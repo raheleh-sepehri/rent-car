1f014dd07e13b2b302df0497bf05d3cf471a282d
Raheleh sepehri <raheleh1392@gmail.com>
2023-08-11 19:17:06 +0330

    add type of SearchManufactureProps

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,14 @@
-import { MouseEventHandler } from "react";
+import { Dispatch, MouseEventHandler, SetStateAction } from "react";
 
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit" | "reset" 
+}
+
+export interface SearchManufactureProps{
+  manufacture:string;
+  setManufacture:Dispatch<SetStateAction<string>>
+  
 }
