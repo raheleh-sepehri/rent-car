15df9ed003df4cd07ee86257412a51d71812dc53
Raheleh sepehri <raheleh1392@gmail.com>
2023-08-11 16:34:16 +0330

    develope Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100 ">
+      <div className="flex max-md:flex-coll flex-wrap p-1 justify-between gap-5 padding-x py-10   ">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="text-base text-gray-700 ">
+            Carhub 2023 <br /> All rights reserved &copy.
+          </p>
+        </div>
 
-export default Footer
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link key={item.title} href={item.url}>
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between items-center mt-10 border-t border-gray-100 padding-x py-10 ">
+        <p>@2023. All Rights Reserved </p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500 ">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500 ">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
