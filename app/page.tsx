8c2713c24063bf5709e4bed05e3673a808c18708
Raheleh sepehri <raheleh1392@gmail.com>
2023-08-11 22:40:27 +0330

    add section to show cars

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
 
 import Image from "next/image";
 
 export default async function Home() {
   const allCars = await fetchCars();
- 
+
+  const isDataEmpty = !allCars || allCars.length < 1 || !Array.isArray(allCars);
 
   return (
     <main className="overflow-hidden w-full">
@@ -22,6 +23,21 @@ export default async function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-xl font-bold text-black">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
